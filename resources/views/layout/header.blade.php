76f44ab8d676b4a23f8d14949f3682d146a69c8f
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 08:34:30 -0400

    fix the style issue with the menubar, had to add <div> container

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,6 +1,7 @@
 <header class="menubar">
-  <ul class="menulink">
-	  <li class="menulink"><a class="menulink"href="#">Home</a></li>
-	  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
-	  <li class="menulink"><a class="menulink" href="#">Business</a></li>
-</ul></header>
+  <div class="menubar">
+ 	  <ul class="menulink">
+		  <li class="menulink"><a class="menulink"href="#">Home</a></li>
+		  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
+		  <li class="menulink"><a class="menulink" href="#">Business</a></li>
+</ul></div></header>
