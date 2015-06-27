0f874f12801fcecc54375a2498eb9b0060ed8bfc
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 09:27:25 -0400

    Edit css and add the contact page

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,7 +1,9 @@
 <header class="menubar">
-  <div class="menubar">
- 	  <ul class="menulink">
-		  <li class="menulink"><a class="menulink"href="#">Home</a></li>
-		  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
-		  <li class="menulink"><a class="menulink" href="#">Business</a></li>
+<div class="menubar">
+  <ul class="menulink">
+	  <li class="menulink"><a class="menulink" href="index">Home</a></li>
+	  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
+	  <li class="menulink"><a class="menulink" href="#">Business</a></li>
+	  <li class="menulink"><a class="menulink" href="contact">Contact</a></li>
+
 </ul></div></header>
