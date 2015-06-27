df60c8d02a2abc1280b67e9cce5675b94ede3988
Jason Ohr <jason.ohr@gmail.com>
2015-06-26 23:56:38 -0400

    I have the increment/decrement working

--- scripts/views/attendantView.js
+++ scripts/views/attendantView.js
@@ -3,16 +3,31 @@
  */
 var AttendantView =Backbone.View.extend({
     template: JST.attendant,
-    className: 'container',
+    className: 'attendant-container',
     events: {
-        'submit': 'postMessage'
+        'click .attendant-car-in': 'carIn',
+        'click .attendant-car-out': 'carOut'
     },
     initialize: function(){
+        this.cars = 30;
         this.render();
     },
 
     render: function(){
-        this.$el.html(this.template());
+        this.$el.html(JST['attendant']({
+            "cars":this.cars
+        }));
+    },
+    carIn: function(){
+        this.cars = $('.attendant-spots-left').text();
+        this.cars = parseInt(this.cars) + 1;
+        this.render(this.cars);
+    },
+    carOut: function(){
+        this.cars = $('.attendant-spots-left').text();
+        this.cars = parseInt(this.cars) - 1;
+        this.render(this.cars);
+
     }
 
 });
