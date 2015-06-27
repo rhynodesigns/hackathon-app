24a3f085335b312bf4d2cd84519b5d1782f15525
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 14:34:32 -0400

    Add html folder with all the html files for index.html

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -1,7 +1,6 @@
-<html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"> 
-	<title>Parking</title>
-	<link rel="stylesheet" href="style.css">
-</head>
+<html lang="en-US"><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"><title>Parking</title><link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"> <link rel="apple-touch-icon" href="images/template/engage.png"/>
+<meta name="apple-mobile-web-app-capable" content="yes" /></head>
 <body><div id="body">
 @include('layout.header')
   <div class="contents"> @yield('content') </div>
