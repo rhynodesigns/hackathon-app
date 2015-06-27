b02ab0bab705393dd69f52dd1a073c1bd821586b
Ryan McAllen <rhyno@rhynodesigns.com>
2015-06-27 15:04:16 -0400

    Failed build....taking it out.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -40,17 +40,5 @@ class RouteServiceProvider extends ServiceProvider
         $router->group(['namespace' => $this->namespace], function ($router) {
             require app_path('Http/routes.php');
         });
-
-        App::before(function ($request) {
-            if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-
-                header('Access-Control-Allow-Origin', 'http://mydomain.com');
-                header('Allow', 'GET, POST, OPTIONS');
-                header('Access-Control-Allow-Headers', 'Origin, Content-Type, Accept, Authorization, X-Request-With');
-                header('Access-Control-Allow-Credentials', 'true');
-
-                exit;
-            }
-        });
     }
 }
