3957f2839170c47454f60d95c1a7f5e0f7c9581d
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 15:22:00 -0400

    fix the links in the header menubar

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,8 +1,8 @@
 <header class="menubar"><div class="menubar">
 <a href="hackathon.html"><img id="logo" src="img/greenvilleparking.gif"></img></a>
   <ul class="menulink">
-	  <li class="menulink"><a class="menulink" href="hackathon.html">Home</a></li>
-	  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
-	  <li class="menulink"><a class="menulink" href="#">Business</a></li>
- 	  <li class="menulink"><a class="menulink" href="hackathon.html">Park Now</a></li>
+	  <li class="menulink"><a class="menulink" href="/">Home</a></li>
+	  <li class="menulink"><a class="menulink" href="/#/attendants">Attendants</a></li>
+	  <li class="menulink"><a class="menulink" href="/#/business">Business</a></li>
+ 	  <li class="menulink"><a class="menulink" href="austineady.github.io/hackathon-app/">Park Now</a></li>
 </ul></div></header>
