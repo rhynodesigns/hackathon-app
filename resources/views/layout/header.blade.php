257349ec5b5ce4f6224bd1c3cc4936bd154f0838
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 12:09:37 -0400

    added logo into the menu bar, looks nice

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,10 +1,9 @@
 <header class="menubar">
-<img id="logo" src="img/greenvilleparking.gif"></img>
 <div class="menubar">
+<img id="logo" src="img/greenvilleparking.gif"></img>
   <ul class="menulink">
-	  <li class="menulink"><a class="menulink" href="/">Home</a></li>
+	  <li class="menulink"><a class="menulink" href="hackathon.html">Home</a></li>
 	  <li class="menulink"><a class="menulink" href="#">Attendance</a></li>
 	  <li class="menulink"><a class="menulink" href="#">Business</a></li>
-	  <li class="menulink"><a class="menulink" href="contact">Contact</a></li>
-
+ 	  <li class="menulink"><a class="menulink" href="contact.html">Contact</a></li>
 </ul></div></header>
