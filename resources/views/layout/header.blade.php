f790cc40fe7a0a817b8a8263afb56d3e5b7284e9
aaronverhoeven <bzchickin@gmail.com>
2015-06-27 15:43:41 -0400

    fix links yet again

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -2,7 +2,7 @@
 <a href="/"><img id="logo" src="img/greenvilleparking.gif"></img></a>
   <ul class="menulink">
 	  <li class="menulink"><a class="menulink" href="/">Home</a></li>
-	  <li class="menulink"><a class="menulink" href="/#/attendants">Attendants</a></li>
-	  <li class="menulink"><a class="menulink" href="/#/business">Business</a></li>
+	  <li class="menulink"><a class="menulink" href="http://austineady.github.io/hackathon-app/#/attendants">Attendants</a></li>
+	  <li class="menulink"><a class="menulink" href="http://austineady.github.io/hackathon-app/#/business">Business</a></li>
  	  <li class="menulink"><a class="menulink" href="http://austineady.github.io/hackathon-app/">Park Now</a></li>
 </ul></div></header>
