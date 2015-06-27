cf8b5d20a6e0549cfd2fcb9981d4832e541d74d2
Ryan McAllen <rhyno@rhynodesigns.com>
2015-06-27 06:57:43 -0400

    Forgot layout files

---
+++ resources/views/layout/default.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+    <title>Greenville Parking</title>
+
+    <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+</head>
+<body>
+<div class="container">
+  <header> @include('layout.header') </header>
+  <div class="contents"> @yield('content') </div>
+  <footer> @include('layout.footer') </footer>
+</div>
+</body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,6 @@
+<ul>
+  <li><a href="{{URL::to('/')}}">Home</a></li>
+  <li><a href="{{URL::to('/about')}}">About</a></li>
+  <li><a href="{{URL::to('/lots')}}">Lots</a></li>
+  <li><a href="{{URL::to('/login')}}">Login</a></li>
+</ul>
