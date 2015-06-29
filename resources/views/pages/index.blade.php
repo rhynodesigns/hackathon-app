caf2caf5fb85ce81dab151a890d6b95fa5350728
Ryan McAllen <rhyno@rhynodesigns.com>
2015-06-29 16:32:12 -0400

    Add notice of 'Hack-a-thon" so people don't expect the real deal (yet)

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,22 @@
 @extends('layout.default')
 
 @section('content')
+
 <div class="body" >
+<h>Hack-a-thon Prototype</h>
+
+<p>Thanks for @imagineupstate for hosting an awesome Hack-a-thon.</p>
+<p>This site represents the efforst of Austin, Jason, Aaron, Harrison, Matt, and Ryan to create a functional protype of a mobile-friendly web app to allow people looking for parking downtown to find private lots and chose based on location/price and reserve a space at a desired lot.</p>
+
+<hr>
+<br />
+
 <h>About</h>
-<p>Greenville Parking is a system that incorporates an app based user interface as well as a mobile web-app based company and attendant interface to facilitate finding and reserving parking downtown. The system will save time and help parking attendants avoid turning away customers, while also helping users find available parking nearby where they need to be, and know how much it will cost. <br><br>Users are able to reserve and pre-pay for a parking spot, and the attendant will get a notification to be able to set aside parking spots for those pre-purchased.</p>
+<p>Greenville Parking is a system that incorporates an app based user interface as well as a mobile web-app based company and attendant interface to facilitate finding and reserving parking downtown.</p>
+
+<p>The system will save time and help parking attendants avoid turning away customers, while also helping users find available parking nearby where they need to be, and know how much it will cost.</p>
+
+<p>People looking for parking downtown are able to reserve and pre-pay for a parking spot, and the attendant will get a notification to be able to set aside parking spots for those pre-purchased.</p>
 </div>
+
 @stop
